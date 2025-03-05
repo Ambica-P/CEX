7cf55f82caff1adbe5d994951fe7a4671ab21e77
Ambica-P <nonip2601@gmail.com>
2025-03-06 02:03:40 +0530

    update routs.ts

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useSession } from "next-auth/react"
+import { use } from "react"
 export default function(){
+    const session=useSession();
     return (
     <div>
         Dashboard
+        {JSON.stringify(session.data?.user)}
     </div>
     )
 
